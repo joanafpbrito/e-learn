273cf7733e161f6e716dc17a9af30651987e9989
patriciaamarquess <patriciamarquesdc@gmail.com>
2025-01-03 16:31:24 +0000

    erro

--- e-learning/src/pages/ErrorPage.jsx
+++ e-learning/src/pages/ErrorPage.jsx
@@ -2,7 +2,7 @@ function ErrorPage () {
     return (
        
         <div className="Error">
-        <h1>We lost this page</h1>
+        <h1>Estás perdido?</h1>
         <p>We searched high and low but couldn't find what you're looking for.</p>
         <p>Let's find a better place for you to go.</p>
         <a href="/"><button >Go Home</button></a>
