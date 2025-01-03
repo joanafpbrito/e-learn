ae63783b4f33e9f5400caed7d59dae2dea6a3bec
patriciaamarquess <patriciamarquesdc@gmail.com>
2025-01-03 14:02:15 +0000

    act 03/01/25, 14:02

---
+++ e-learning/src/componentsRoot/Banner.jsx
@@ -0,0 +1,33 @@
+import banner180 from '../assets/banner180.png';
+
+function Banner() {
+    const userName = localStorage.getItem("name");
+    const role = localStorage.getItem("role");
+    let message = "";
+    if ( role === 'admin') {
+        message = "A plataforma está nas suas mãos. Pronto para liderar a gestão da aprendizagem?";
+    }
+    if ( role === 'user') {
+        message = "O conhecimento está nas suas mãos. Pronto para investir em si e construir novas competências?";
+    }
+    if ( role === '180') {
+        message = "1805. Pronto para liderar a gestão da aprendizagem?";
+    }
+    if ( role === 'teacher') {
+        message = "O conhecimento está nas suas mãos. Pronto para criar conteúdo e acompanhar os seus formandos?";
+    }
+            
+            
+    return (
+        <>
+            <img src={banner180} alt="image-banner" />
+            <h2>Olá, {userName ? userName : "180"}!</h2>
+            {message}
+
+        </>
+
+    )
+
+}
+
+export default Banner;
--- e-learning/src/componentsRoot/Header.jsx
+++ e-learning/src/componentsRoot/Header.jsx
@@ -3,16 +3,16 @@ import Logout from "../components/Logout"
 import "./rootLayout.css";
 import { AiOutlineLogout } from "react-icons/ai";
 import { IoSettingsOutline } from "react-icons/io5";
-import { FaUser } from "react-icons/fa6";
-import logo180Academy from "../assets/logo180academy.png";
+import { CiUser } from "react-icons/ci";
+import logo180black from "../assets/logo180black.png";
 
 function Header () {
     return (
         <>
             <header className="header">
-                <div className="logo"><Link to ="/"> <img  src= {logo180Academy} alt=""/> </Link></div>
+                <div className="logo"><Link to ="/"> <img  src= {logo180black} alt=""/> </Link></div>
                 <nav className="header-icons">
-                <Link to ="/"> <FaUser/> </Link>
+                <Link to ="/"> <CiUser /> </Link>
                 <Link to = "/"> <IoSettingsOutline /> </Link>
                 <a href= "/" onClick={() => Logout()}>  <AiOutlineLogout/> </a> 
                 </nav>
