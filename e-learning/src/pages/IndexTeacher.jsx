7a35420bb4bf5e9828056f32bd2161415f829b12
Joana Brito <joanafpbrito@gmail.com>
2025-01-06 09:04:20 +0000

    sidebaradmin

--- e-learning/src/pages/IndexTeacher.jsx
+++ e-learning/src/pages/IndexTeacher.jsx
@@ -5,53 +5,11 @@ import CourseList from "../components/CoursesList"
 
 function IndexTeacher () {
     const userName=localStorage.getItem("name");
-    const [banner, setBanner] = useState(true);
-    const [newUser, setNewUser] = useState();
-    const [courseList, setCourseList] =useState();
-
-    function showRegisterNewUser () {
-      setNewUser(true);
-      setBanner(false);
-      setCourseList(false);
-    }
-
-    function showCourseList() {
-        setCourseList(true);
-        setBanner(false);
-        setNewUser(false);
-    }
     return (
-        <>
-        <aside className="sidebar">
-            <div>
-                <p onClick={showRegisterNewUser} > <strong>Register New User</strong> </p>
-                <br />
-                <p onClick={showCourseList}> <strong>Mostrar Cursos</strong> </p>
-                <br />
-                <br />
-                <br />
-                <br /> 
-            </div>
-        </aside>
-        <p>ola {userName ? userName : "180"}</p>
-
-        {banner && 
-            <div>
-                <Banner/> 
-            </div> 
-        }
-        {newUser && 
-            <div>
-                <RegisterNewUser/> 
-            </div> 
-        }
-        {courseList &&
-            <div>
-                <CourseList/> 
-            </div>
-        }
-        
-        </>
+       <div> 
+       <p>ola {userName ? userName : "180"}</p> 
+       <Banner/>
+       </div>
     )
 }
 
