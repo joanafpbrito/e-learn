54751f35a2582c08ba46f3d8d470055cd6738595
Joana Brito <joanafpbrito@gmail.com>
2025-01-07 15:43:08 +0000

    act

--- e-learning/src/pages/IndexModules.jsx
+++ e-learning/src/pages/IndexModules.jsx
@@ -1,11 +1,16 @@
 import CoursePage from "../components/CoursePage";
+import { useParams } from "react-router-dom";
 
 function IndexModules () {
-    
+    let params = useParams()
+   
+    console.log(params.id);
+  
     return (
         <>
         <p>ola modulos</p>
-        <CoursePage/>
+        <CoursePage
+        props = {params.id}/>
         </>
     )
 }
